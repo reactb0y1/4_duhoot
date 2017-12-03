aea0fb1cb82714657690558dfce67dd841746a81
sashakobzeww <sashakobzeww@gmail.com>
2017-12-03 12:06:31 +0200

    Начинаем делать блок 'filter'

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,36 @@ var uglify = require('gulp-uglify');			// Сжимаем JS
 var csso = require('gulp-csso');				// Сжимаем CSS
 var prefixer = require("gulp-autoprefixer");	// Добавляем префиксы
 var uncss = require('gulp-uncss');				// Очишаем CSS от лишнего кода
-var livereload = require('gulp-livereload');	
+var less = require('gulp-less');				// Для препроцессора
+// var path = require('path');
+// var connect = require('gulp-connect'); 				//livereload
+var livereload = require('gulp-livereload');
+
+/*
+// Локальный сервер для разработки
+gulp.task('connect', function(){
+	connect.server({ //настриваем конфиги сервера
+		root: ['../'], //корневая директория запуска сервера
+		port: 9999, //какой порт будем использовать
+		livereload: true //инициализируем работу LiveReload
+	});
+});*/
 
 // CSS
 gulp.task('CSS', function () {
-	return gulp.src('src/**/*.css')
+	return gulp.src('src/**/*.less')
+		.pipe(less())
 		.pipe(concat("style.css"))			// Склеиваем CSS
-		.pipe(uncss({						// Чистим от ичшнего кода 
+		.pipe(uncss({						// Чистим от лишнего кода 
 			html: ['build/index.html']
 		}))
 		.pipe(prefixer({					// Добавляем префиксы
 			browsers: ['last 3 version', "> 1%", "ie 8", "ie 7"]
 		}))
-		.pipe(csso())					// Минифиируем CSS
-		.pipe(gulp.dest('build/'));
+		// .pipe(csso())					// Минифиируем CSS
+		.pipe(gulp.dest('build/'))
+		// .pipe(connect.reload());
+		// .pipe(livereload());
 });
 
 // JS
@@ -26,12 +42,15 @@ gulp.task('JS', function () {
 	return gulp.src('src/**/*.js')
 		.pipe(concat("common.js"))		// Склеиваем JS
 		.pipe(uglify())					// Минификация JS
-		.pipe(gulp.dest('build/'));
+		.pipe(gulp.dest('build/'))
+		// .pipe(connect.reload());
+		// .pipe(livereload());
 });
 
 // Отслеживание изменений
 gulp.task('watch', function () {
-  return gulp.watch('src/**/*', ['CSS', 'JS']);
+  // livereload.listen();
+  gulp.watch('src/**/*', ['CSS', 'JS']);
 });
 
 // Функция по умолчанию
@@ -40,6 +59,13 @@ gulp.task("default", ['CSS', 'JS', "watch"])
 
 /*
 
+npm i gulp-imagemin -D
+npm i gulp-connect -D
+npm install gulp-less
+	LESS
+	Less2Css
+
+Настроить отслеживание изменений в html-файле
 
 gulp-minify-css - этот минификатор не устанавливать - он нарушает пути
 локальных файлов
